87facfac335ca561f6a36e0903db2f90895b0a8a
rcroft1 <rcroft1@byu.edu>
2023-02-22 13:31:10 -0700

    1st push

---
+++ Project-2-TaskApp/Models/TaskData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_2_TaskApp.Models
+{
+    public class TaskData
+    {
+        [Required]
+        public string taskName { get; set; }
+        public DateTime dueDate { get; set; }
+        [Required]
+        public int quadrant { get; set; }
+        public string category { get; set; }
+        public bool completed { get; set; }
+    }
+}
